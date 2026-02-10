ea0bb403d9f30dafbab656152d9cc8c2c92433e8
krsns <98344484+krsns@users.noreply.github.com>
2026-02-10 07:24:38 +0700

    Update create_accounts.py

--- create_accounts.py
+++ create_accounts.py
@@ -1,13 +1,22 @@
 import requests, json, time
 from config import BASE_URL, TOTAL_ACCOUNTS
 
+prefix = input("Masukkan prefix nama bot (contoh: vps2_ atau userA_): ").strip()
+if not prefix:
+    prefix = "bot_"
+
+start = input("Mulai dari nomor berapa? (default 1): ").strip()
+start = int(start) if start else 1
+
 accounts = []
 
 for i in range(TOTAL_ACCOUNTS):
-    name = f"kewan_{i+1}"
+    name = f"{prefix}{start + i}"
+
     r = requests.post(
         f"{BASE_URL}/accounts",
-        json={"name": name}
+        json={"name": name},
+        timeout=20
     ).json()
 
     api_key = r["data"]["apiKey"]
